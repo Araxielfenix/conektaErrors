0bfb13b863a60bb7c2d82508b7a00f3d7a0eabb9
Araxielfenix <araxielfenix@outlook.com>
2022-06-02 16:28:15 -0500

    Conversión de JSON a String y busqueda de error por tecla presionada.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,23 @@
-//Aquí va el escript
+var errorInput = document.getElementById("css-input").value;
+/**
+ * It takes a string, turns it into an object, then searches for the string in the object, and returns
+ * the error message.
+ * @param errorInput - The error message that you want to search for.
+ * @returns The error message.
+ */
+function getError(errorInput){
+    // Turn JSON string into object.
+    var errorObject = JSON.parse(errorInput);
+    // Foreach error in the object.
+    for (var i = 0; i < errorObject.errors.length; i++) {
+        //Search for errorInput in errorObject.errors[i].message.
+        if (errorObject.errors[i].message.indexOf(errorInput) > -1) {
+            // Return the error message.
+            return errorObject.errors[i].message;
+        }
+    }
+}
+// e.keypress in the css-input field.
+document.getElementById("css-input").addEventListener("keypress", function(e){
+    getError();
+});
